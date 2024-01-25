6dee10543dddad2320b327fdfd20d7cb11f63218
aniimessh <animeshgupta2.002@gmail.com>
2024-01-25 23:34:19 +0530

    fix: studynotion project details

--- data/constants.js
+++ data/constants.js
@@ -74,16 +74,17 @@ export const ProjectImageData = [
     path: "/assets/project/StudyNotion-Project.png",
     projectDetails: (
       <div className="font-Nunito">
-        🚀 <strong>First internship project</strong>: Unveiling a dynamic,
-        user-friendly web app. <br />
-        🎨 <strong>Visual Brilliance</strong>: Crafted a sleek frontend using
-        HTML, CSS, and JavaScript. <br />
-        🧩 <strong>Reusable Wonders</strong>: Engineered modular components for
-        swift development and scalability. <br />
-        🌐 <strong>API Integration</strong>: Unleashed real-time magic,
-        enriching user experience <br />
-        🏆 <strong>Best Practices Champ</strong>: Committed to clean, efficient
-        code, setting industry standards.
+        🚀 <strong>Personalized Learning</strong>: StudyNotion leverages AI
+        algorithms to provide personalized learning experiences tailored to each
+        student's unique needs and learning style. <br />
+        🎨 <strong>Interactive Content</strong>: The platform offers engaging
+        and interactive learning materials, including videos, quizzes, and
+        simulations, to enhance student engagement and comprehension. <br />
+        🧩 <strong>Collaboration Features</strong>: Studynotion encourages
+        collaboration among students by providing features such as discussion
+        forums, group projects, and fostering a sense of community and knowledge
+        sharing.
+        <br />
       </div>
     ),
     link: "http://jettradefx.in/",
