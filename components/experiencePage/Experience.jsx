f3eeec9c10574fc8bf384af1e0dba0b55c4e801c
aniimessh <animeshgupta2.002@gmail.com>
2024-02-04 02:25:33 +0530

    feat: work experience added

--- components/experiencePage/Experience.jsx
+++ components/experiencePage/Experience.jsx
@@ -6,13 +6,21 @@ import {
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
 import { IoSchoolOutline } from "react-icons/io5";
-import { LuSchool } from "react-icons/lu";
+import { educationDetails } from "@/utils/constants";
+import { useState } from "react";
+import { BsPersonWorkspace } from "react-icons/bs";
 
 const Experience = () => {
+  const [selectedQualification, setSelectedQualification] =
+    useState("education");
+
+  const handleQualificationChange = (event) => {
+    setSelectedQualification(event.target.value);
+  };
   return (
     <>
       <div
-        className="sm:w-3/4 pt-20 sm:pt-0 p-5 sm:p-0 w-full md:h-screen  mx-auto flex justify-center items-center flex-col"
+        className="sm:w-3/4 pt-20 pb-20 sm:pt-0 p-5 sm:p-0 w-full md:h-screen  mx-auto flex justify-center items-center flex-col"
         id=""
       >
         <>
@@ -21,72 +29,120 @@ const Experience = () => {
           <p className="text-lg w-full mt-5 font-Nunito font-semibold text-center">
             My Educational Qualification & Work Experience
           </p>
+          <div className="flex gap-x-6 mt-4">
+            <label
+              htmlFor="academic"
+              className={`font-Nunito ${
+                selectedQualification === "education"
+                  ? "opacity-100"
+                  : "opacity-50"
+              } cursor-pointer`}
+            >
+              Education
+            </label>
+            <input
+              type="radio"
+              id="academic"
+              name="qualification"
+              value="education"
+              defaultChecked
+              onChange={handleQualificationChange}
+              className="appearance-none"
+            />
+            <label
+              htmlFor="work"
+              className={`font-Nunito ${
+                selectedQualification === "work" ? "opacity-100" : "opacity-50"
+              } cursor-pointer`}
+            >
+              Work
+            </label>
+            <input
+              type="radio"
+              id="work"
+              name="qualification"
+              value="work"
+              onChange={handleQualificationChange}
+              className="appearance-none"
+            />
+          </div>
         </>
-        <div className="text-black w-full">
-          <VerticalTimeline>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work text-black"
-              contentStyle={{
-                color: "#000",
-                boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-                border: "1px solid #0f0f0f",
-              }}
-              contentArrowStyle={{
-                borderRight: "7px solid  rgba(0, 0, 0, 0.15)",
-                boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-                border: "1px solid #0f0f0f",
-              }}
-              date=" 2021 - 2024"
-              iconStyle={{
-                color: "#000",
-                boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-                border: "1px solid #0f0f0f",
-              }}
-              icon={<IoSchoolOutline />}
-            >
-              <h3
-                className="vertical-timeline-element-title"
-                style={{ color: "black" }}
+        <div className="text-black w-full h-2/4">
+          {selectedQualification === "education" && (
+            <VerticalTimeline>
+              {educationDetails.map((item) => {
+                return (
+                  <VerticalTimelineElement
+                    key={item.id}
+                    className="vertical-timeline-element--work text-black"
+                    contentStyle={{
+                      color: "#000",
+                      boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                      border: "1px solid #0f0f0f",
+                    }}
+                    contentArrowStyle={{
+                      borderRight: "7px solid  rgba(0, 0, 0, 0.15)",
+                      boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                      border: "1px solid #0f0f0f",
+                    }}
+                    date={item.year}
+                    iconStyle={{
+                      color: "#000",
+                      boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                      border: "1px solid #0f0f0f",
+                    }}
+                    icon={item.icon}
+                  >
+                    <h3
+                      className="vertical-timeline-element-title"
+                      style={{ color: "black" }}
+                    >
+                      {item.collegeName}
+                    </h3>
+                    <h4 className="vertical-timeline-element-subtitle">
+                      {item.place}
+                    </h4>
+                    <p>{item.course}</p>
+                  </VerticalTimelineElement>
+                );
+              })}
+            </VerticalTimeline>
+          )}
+          {selectedQualification === "work" && (
+            <VerticalTimeline>
+              <VerticalTimelineElement
+                className="vertical-timeline-element--work text-black"
+                contentStyle={{
+                  color: "#000",
+                  boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                  border: "1px solid #0f0f0f",
+                }}
+                contentArrowStyle={{
+                  borderRight: "7px solid  rgba(0, 0, 0, 0.15)",
+                  boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                  border: "1px solid #0f0f0f",
+                }}
+                date="10th-Jul'23 - 4th-Dec'23"
+                iconStyle={{
+                  color: "#000",
+                  boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
+                  border: "1px solid #0f0f0f",
+                }}
+                icon={<BsPersonWorkspace />}
               >
-                OP Jindal University
-              </h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Raigarh, Chattisgarh
-              </h4>
-              <p>B.Tech in Computer Science & Engineering</p>
-            </VerticalTimelineElement>
-            <VerticalTimelineElement
-              className="vertical-timeline-element--work text-black"
-              contentStyle={{
-                color: "#000",
-                boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-                border: "1px solid #0f0f0f",
-              }}
-              contentArrowStyle={{
-                borderRight: "7px solid rgba(0, 0, 0, 0.15) ",
-                boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-                border: "1px solid #0f0f0f",
-              }}
-              date=" 2018  - 2021"
-              iconStyle={{
-                color: "#000",
-                boxShadow: "rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px",
-                border: "1px solid #0f0f0f",
-              }}
-              icon={<LuSchool />}
-            >
-              <h3
-                className="vertical-timeline-element-title"
-                style={{ color: "black" }}
-              >
-                Usha Martin Unversity
-              </h3>
-              <h4 className="vertical-timeline-element-subtitle">
-                Ranchi, Jharkhand
-              </h4>
-              <p>Diploma in Computer Science & Engineering</p>
-            </VerticalTimelineElement>
-          </VerticalTimeline>
+                <h3
+                  className="vertical-timeline-element-title"
+                  style={{ color: "black" }}
+                >
+                  Centum World
+                </h3>
+                <h4 className="vertical-timeline-element-subtitle">
+                  Kolkata, West Bengal
+                </h4>
+                <p>Software Developer Intern</p>
+              </VerticalTimelineElement>
+            </VerticalTimeline>
+          )}
         </div>
       </div>
     </>
---
+++ utils/constants.js
@@ -0,0 +1,21 @@
+import { LuSchool } from "react-icons/lu";
+import { IoSchoolOutline } from "react-icons/io5";
+
+export const educationDetails = [
+    {
+        id: 1,
+        collegeName: "OP Jinal University",
+        year: "2021- 2024",
+        place: "Raigarh, Chattisgarh",
+        course: "B.Tech in Computer Science",
+        icons: <IoSchoolOutline />
+    },
+    {
+        id: 2,
+        collegeName: "Usha Martin University",
+        year: "2018- 2021",
+        place: "Ranchi, Jharkhand",
+        course: "Diploma in Computer Science",
+        icon: <LuSchool/>
+    },
+]
