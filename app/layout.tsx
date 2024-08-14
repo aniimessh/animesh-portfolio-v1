9d4fa58593afe6b2aca00f30e5f75470409c377b
aniimessh <animeshgupta2.002@gmail.com>
2024-08-14 12:56:51 +0530

    project reinitalized

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "../globals.css";
-import Navbar from "@/components/Navbar";
+import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,12 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} w-full`}>
-        <section className="container mx-auto">
-          <Navbar />
-          {children}
-        </section>
-      </body>
+      <body className={inter.className}>{children}</body>
     </html>
   );
 }
